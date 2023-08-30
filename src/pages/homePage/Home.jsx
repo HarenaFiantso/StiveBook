740055b17bdfb487bbd8ebec70f357db2dbe5203
Fiantso Harena <fiantsorav@gmail.com>
2023-08-31 02:24:42 +0300

    feat: Create the main implementation of the share component

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,7 +1,15 @@
+import Navbar from "../../components/navbar/Navbar";
+import Share from "../../components/share/Share";
+
 import "./home.css";
 
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <Navbar />
+      <Share />
+    </div>
+  );
 }
 
 export default Home;
