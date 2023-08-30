5d3c6279d5d82a5547fa15b3760ef9ff3151e6c7
Fiantso Harena <fiantsorav@gmail.com>
2023-08-30 22:25:35 +0300

    chore: Implement a static page profile

--- src/pages/profilePage/Profile.jsx
+++ src/pages/profilePage/Profile.jsx
@@ -2,28 +2,27 @@ import {
   EmailOutlined,
   Facebook,
   Instagram,
-  Language,
   LinkedIn,
-  LocationCity,
   MoreVert,
   Pinterest,
 } from "@mui/icons-material";
 import profileImage from "../../assets/1.jpg";
 import coverImage from "../../assets/2.jpg";
+import Navbar from "../../components/navbar/Navbar";
 
 import "./profile.css";
 
 function Profile() {
   return (
-    <div className="profile">
-      <>
+    <>
+      <Navbar />
+      <div className="profile">
         <div className="images">
           <img src={coverImage} alt="profile Image" className="cover" />
           <img src={profileImage} alt="profile Image" className="profilePic" />
         </div>
         <div className="profileContainer">
           <div className="uInfo">
-            
             {/* Ceci est juste un lien optionnel que j'ai ajouté pour juste ajouter un style à la page */}
             <div className="left">
               <a href="https://web.facebook.com/fiantso.harena.3/">
@@ -39,14 +38,12 @@ function Profile() {
                 <Pinterest fontSize="large" />
               </a>
             </div>
-
             {/* Ici ce sont les informations supplémentaire */}
             <div className="center">
               <span className="centerTitle">Fiantso Harena</span>
               <span>God's worshiper</span>
-              <button>Welcome</button>
+              <button>Update my profile</button>
             </div>
-
             {/* Mmmh, some additionnal features I think */}
             <div className="right">
               <EmailOutlined />
@@ -54,8 +51,8 @@ function Profile() {
             </div>
           </div>
         </div>
-      </>
-    </div>
+      </div>
+    </>
   );
 }
 
