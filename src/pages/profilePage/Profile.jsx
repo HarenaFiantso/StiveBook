8b9104e2af6c57febb3e8130e195854f188d0538
Fiantso Harena <fiantsorav@gmail.com>
2023-08-30 20:48:12 +0300

    style: Styling the profile page

--- src/pages/profilePage/Profile.jsx
+++ src/pages/profilePage/Profile.jsx
@@ -1,4 +1,13 @@
-import { Facebook, Instagram, Language, LinkedIn, LocationCity, Pinterest } from "@mui/icons-material";
+import {
+  EmailOutlined,
+  Facebook,
+  Instagram,
+  Language,
+  LinkedIn,
+  LocationCity,
+  MoreVert,
+  Pinterest,
+} from "@mui/icons-material";
 import profileImage from "../../assets/1.jpg";
 import coverImage from "../../assets/2.jpg";
 
@@ -14,6 +23,7 @@ function Profile() {
         </div>
         <div className="profileContainer">
           <div className="uInfo">
+            
             {/* Ceci est juste un lien optionnel que j'ai ajouté pour juste ajouter un style à la page */}
             <div className="left">
               <a href="https://web.facebook.com/fiantso.harena.3/">
@@ -30,18 +40,17 @@ function Profile() {
               </a>
             </div>
 
+            {/* Ici ce sont les informations supplémentaire */}
             <div className="center">
-              <span>Fiantso Harena</span>
-              <div className="info">
-                <div className="item">
-                  <LocationCity />
-                  <span>Antsirabe</span>
-                </div>
-                <div className="item">
-                  <Language />
-                  <span>www.fiantso.com</span>
-                </div>
-              </div>
+              <span className="centerTitle">Fiantso Harena</span>
+              <span>God's worshiper</span>
+              <button>Welcome</button>
+            </div>
+
+            {/* Mmmh, some additionnal features I think */}
+            <div className="right">
+              <EmailOutlined />
+              <MoreVert />
             </div>
           </div>
         </div>
