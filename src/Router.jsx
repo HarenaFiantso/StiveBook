d32365690b6a924f05737b905358b104ae637b8f
Fiantso Harena <fiantsorav@gmail.com>
2023-08-31 20:07:51 +0300

    fix: Added the register page to the route

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,14 +3,14 @@ import ProtectedRoute from "./utils/ProtectedRoute.jsx";
 
 import Profile from "./pages/profilePage/Profile";
 import Login from "./pages/loginPage/Login";
-import Signup from "./pages/RegisterPage/Register";
+import Register from "./pages/registerPage/Register";
 import Home from "./pages/homePage/Home";
 
 function Router() {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/register" element={<Register />} />
             <Route element={<ProtectedRoute/>}>
                 <Route path="/" element={<Home />} />
                 <Route path="/profile/:id" element={<Profile />} />
