ea14201299c763d02530a1a6bf4beec9e508df46
Antenaina2002 <hei.antenaina@gmail.com>
2023-08-31 12:05:15 +0200

    feat: added authpage, Login and SignUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import Profile from "./pages/profilePage/Profile";
 import Home from "./pages/homePage/Home";
+import Login from "./pages/authpage/Login"
+import SignUp from "./pages/authpage/SignUp"
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/Login" element={<Login/>}/>
+          <Route path="/SignUp" element={<SignUp/>}/>
         </Routes>
       </Router>
     </>
