84cc29cc760d4692fcfb321d511bcc50da850243
Antenaina2002 <hei.antenaina@gmail.com>
2023-08-31 12:19:22 +0200

    Revert "feat: added authpage, Login and SignUp"
    This reverts commit ea14201299c763d02530a1a6bf4beec9e508df46.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Profile from "./pages/profilePage/Profile";
 import Home from "./pages/homePage/Home";
-import Login from "./pages/authpage/Login"
-import SignUp from "./pages/authpage/SignUp"
 
 function App() {
   return (
@@ -12,8 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile/:id" element={<Profile />} />
-          <Route path="/Login" element={<Login/>}/>
-          <Route path="/SignUp" element={<SignUp/>}/>
         </Routes>
       </Router>
     </>
