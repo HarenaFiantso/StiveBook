72d3fcc89772401699fbb8b5619d35d6560949b8
Fiantso Harena <fiantsorav@gmail.com>
2023-08-31 21:17:20 +0300

    chore: Make some minimal changes

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./utils/ProtectedRoute.jsx";
 
 import Profile from "./pages/profilePage/Profile";
@@ -7,17 +7,17 @@ import Register from "./pages/registerPage/Register";
 import Home from "./pages/homePage/Home";
 
 function Router() {
-    return (
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route element={<ProtectedRoute/>}>
-                <Route path="/" element={<Home />} />
-                <Route path="/profile/:id" element={<Profile />} />
-            </Route>
-            {/*<Route path={"/logout"} element={<Logout/>} />*/}
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Register />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/:id" element={<Profile />} />
+      </Route>
+      {/*<Route path={"/logout"} element={<Logout/>} />*/}
+    </Routes>
+  );
 }
 
 export default Router;
