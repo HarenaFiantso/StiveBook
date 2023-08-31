27e129890824c8fd31664760913b5f1e484d79ee
Fiantso Harena <fiantsorav@gmail.com>
2023-09-01 00:52:56 +0300

    I'm done for today

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
