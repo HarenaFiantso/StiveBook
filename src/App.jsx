1bb55bf50d6b5a2464a7f76c9280cb7a081c3bb4
Antenaina2002 <hei.antenaina@gmail.com>
2023-08-31 14:21:42 +0200

    feat: added authpage, login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
 
 import Profile from "./pages/profilePage/Profile";
 import Home from "./pages/homePage/Home";
+import Login from "./pages/authpage/Login";
+import SignupCard from "./pages/authpage/SignUp";
 
 function App() {
   return (
-    <>
+    <ChakraProvider>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignupCard />} />
         </Routes>
       </Router>
-    </>
+    </ChakraProvider>
   );
 }
 
---
+++ src/pages/authpage/Login.jsx
@@ -0,0 +1,62 @@
+import { Link } from 'react-router-dom';
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+export default function SimpleCard() {
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <Heading textAlign={'center'}>Welcome, please Login</Heading>
+            <FormControl id="email">
+              <FormLabel>Username</FormLabel>
+              <Input type=" " />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Remember me</Checkbox>
+                <Text color={'blue.400'}>Forgot password?</Text>
+              </Stack>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Sign in
+              </Button>
+            </Stack>
+            <Link to="/signup">Don't have an account? Sign Up</Link>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  )
+}
