7b02f916b94a296d1a8ef2cc55868d14336a4af1
Mii <32428593+MiiJack@users.noreply.github.com>
2023-09-01 00:28:12 +0300

    fix : Fixed (finally, finally) the routing

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -7,8 +7,8 @@ const ProtectedRoute = () => {
     const { isConnected } = useContext(UserContext);
 
     useEffect(() => {
-        if (isConnected()) {
-            navigate('/');
+        if (!isConnected()) {
+            navigate('/login');
         }
     }, [navigate, isConnected]);
 
