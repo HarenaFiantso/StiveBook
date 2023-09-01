e801a845c17aa3cf23a990bec50dbb3d08573211
Mii <32428593+MiiJack@users.noreply.github.com>
2023-09-01 09:47:11 +0300

    Feat : Added Page not found and rerouting

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,24 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import ReRoute from "./utils/ReRoute";
-
-import Profile from "./pages/profilePage/Profile";
-import Login from "./pages/loginPage/Login";
-import Register from "./pages/registerPage/Register";
-import Home from "./pages/homePage/Home";
+import{Home, NotFound, Profile, Register, Login} from"./pages/index.js";
 
 function Router() {
   return (
     <Routes>
+        <Route path='*' element={<NotFound />} />
         <Route element={<ReRoute />}>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
         </Route>
-      <Route element={<ProtectedRoute />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile/:id" element={<Profile />} />
-      </Route>
-      {/*<Route path={"/logout"} element={<Logout/>} />*/}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Route>
+        {/*<Route path={"/logout"} element={<Logout/>} />*/}
     </Routes>
   );
 }
